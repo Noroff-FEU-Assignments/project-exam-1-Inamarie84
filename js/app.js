e1aa75732f92105314d2362b09ae4a6f8fdbf071
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2024-06-11 19:13:47 +0200

    added and fixed js files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-import { displayBlogPosts } from "./events/posts/displayBlogposts.js";
+// import { displayBlogPosts } from "./events/posts/displayBlogposts.js";
+
+// const { pathname } = location;
+
+// console.log(pathname);
+
+// switch (pathname) {
+//   case "/":
+//   case "/index.html":
+//     displayBlogPosts();
+//     break;
+// }
+
+import { displayBlogPosts } from "./events/posts/displayBlogPosts.js";
 
 const { pathname } = location;
 
